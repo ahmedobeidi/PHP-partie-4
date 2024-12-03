92e6fd3c74c4c25f62bf9559c36bfb9d473dd2df
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-03 16:50:13 +0100

    DONE: Exercise 3

--- algo.php
+++ algo.php
@@ -11,3 +11,8 @@ function getAndReturnString(string $text) {
     return $text;
 } 
 */ 
+/* Exercise 3 
+function getAndReturnString(string $text1, string $text2) {
+    return "{$text1}{$text2}";
+}
+*/

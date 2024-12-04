c90ec395726fd8d33bb2cdec56e7c1c4e7a56009
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-04 11:21:34 +0100

    DONE: Exercise 4

--- algo.php
+++ algo.php
@@ -11,8 +11,19 @@ function getAndReturnString(string $text) {
     return $text;
 } 
 */ 
+
 /* Exercise 3 
 function getAndReturnString(string $text1, string $text2) {
     return "{$text1}{$text2}";
 }
+*/
+
+/* Exercise 4 
+function compare(int $number1, int $number2) {
+    if ($number1 > $number2) return "Le premier nombre est plus grand";
+    else if ($number1 < $number2) return "Le premier nombre est plus petit";
+    else return "Les deux nombres sont identiques";
+}
+
+echo compare(1, 10);
 */
